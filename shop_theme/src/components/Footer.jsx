cbd21e311384b046183678b6fcdb38d83a3893c3
Adam Chilton <adam@dsan.co.uk>
2020-04-30 15:56:03 +0100

    improved footer

--- shop_theme/src/components/Footer.jsx
+++ shop_theme/src/components/Footer.jsx
@@ -1,11 +1,11 @@
 /** @jsx jsx */
 /*eslint no-unused-vars: 0*/
-import React from 'react';
-import { jsx, useThemeUI } from 'theme-ui';
-import { Flex, Box, Text, Link } from 'rebass';
-import { useStaticQuery, graphql } from 'gatsby';
-import GatsbyLink from 'gatsby-link';
-import { SocialIcon } from 'react-social-icons';
+import React from 'react'
+import { jsx, useThemeUI } from 'theme-ui'
+import { Flex, Box, Text, Link } from 'rebass'
+import { useStaticQuery, graphql } from 'gatsby'
+import GatsbyLink from 'gatsby-link'
+import { SocialIcon } from 'react-social-icons'
 
 const validURL = str => {
   let pattern = new RegExp(
@@ -16,9 +16,9 @@ const validURL = str => {
     '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
       '(\\#[-a-z\\d_]*)?$',
     'i'
-  ); // fragment locator
-  return !!pattern.test(str);
-};
+  ) // fragment locator
+  return !!pattern.test(str)
+}
 
 function Footer() {
   const data = useStaticQuery(graphql`
@@ -42,7 +42,7 @@ function Footer() {
         }
       }
     }
-  `);
+  `)
 
   const {
     email,
@@ -54,11 +54,11 @@ function Footer() {
     workingHours,
     socialNetworks,
     footerLinks,
-  } = data.site.siteMetadata.gatsbyStorefrontConfig;
+  } = data.site.siteMetadata.gatsbyStorefrontConfig
 
-  const { theme } = useThemeUI();
+  const { theme } = useThemeUI()
 
-  const year = new Date().getFullYear();
+  const year = new Date().getFullYear()
 
   return (
     <Box bg="backgroundFooter" py={[1, 2, 3, 4]} mt={[2, 3, 4]}>
@@ -71,68 +71,63 @@ function Footer() {
         px={2}
         pt={3}
       >
-        <Flex alignItems="center" mb={[2, 3, 4]} flexWrap="wrap">
-          <Flex
-            width={[1, 1, 3 / 4]}
-            justifyContent={['center', 'space-between']}
-            flexWrap="wrap"
-            mb={3}
-          >
-            {footerLinks
-              ? footerLinks.map((link, index) => {
-                  // If link is valid url use <a>
-                  // else use gatsby-link
-                  if (validURL(link.link)) {
-                    return (
-                      <Text key={index} mr={[3, 0]} my={[2, 0]}>
-                        <Link href={link.link}>{link.name}</Link>
-                      </Text>
-                    );
-                  } else {
-                    return (
-                      <Text key={index} mr={[3, 0]} my={[2, 0]}>
-                        <GatsbyLink to={link.link} sx={theme.variants.link}>
-                          {link.name}
-                        </GatsbyLink>
-                      </Text>
-                    );
-                  }
-                })
-              : ''}
-          </Flex>
-
-          <Flex
-            width={[1, 1, 1 / 4]}
-            justifyContent={['center', 'center', 'flex-end']}
-            mr="auto"
-            pl={2}
-            mb={3}
-          >
-            {socialNetworks
-              ? socialNetworks.map((socialNetwork, index) => {
+        <Flex
+          justifyContent={['center', 'space-between']}
+          flexWrap="wrap"
+          mb={[3, 3, 4]}
+        >
+          {footerLinks
+            ? footerLinks.map((link, index) => {
+                // If link is valid url use <a>
+                // else use gatsby-link
+                if (validURL(link.link)) {
                   return (
-                    <Box key={index}>
-                      <SocialIcon
-                        url={socialNetwork}
-                        sx={{
-                          height: 48,
-                          width: 48,
-                          marginLeft: [2, 3],
-                          opacity: 0.8,
-                          ':hover,:focus,.active': {
-                            opacity: 1,
-                          },
-                        }}
-                        bgColor={theme.colors.primary}
-                      />
-                    </Box>
-                  );
-                })
-              : ''}
-          </Flex>
+                    <Text key={index} mr={[3, 0]} my={[2, 0]}>
+                      <Link href={link.link}>{link.name}</Link>
+                    </Text>
+                  )
+                } else {
+                  return (
+                    <Text key={index} mr={[3, 0]} my={[2, 0]}>
+                      <GatsbyLink to={link.link} sx={theme.variants.link}>
+                        {link.name}
+                      </GatsbyLink>
+                    </Text>
+                  )
+                }
+              })
+            : ''}
         </Flex>
-
-        <Flex>
+        <Flex
+          justifyContent={'center'}
+          flexWrap={'wrap'}
+          mr="auto"
+          pl={2}
+          mb={[3, 3, 4]}
+        >
+          {socialNetworks
+            ? socialNetworks.map((socialNetwork, index) => {
+                return (
+                  <Box key={index}>
+                    <SocialIcon
+                      url={socialNetwork}
+                      sx={{
+                        height: 48,
+                        width: 48,
+                        marginLeft: [2, 3],
+                        opacity: 0.8,
+                        ':hover,:focus,.active': {
+                          opacity: 1,
+                        },
+                      }}
+                      bgColor={theme.colors.primary}
+                    />
+                  </Box>
+                )
+              })
+            : ''}
+        </Flex>
+        <Flex justifyContent={'center'}>
           <Box>
             <Text fontSize={[1, 2]}>
               © {year} {company ? company : ''}
@@ -145,7 +140,7 @@ function Footer() {
         </Flex>
       </Box>
     </Box>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
