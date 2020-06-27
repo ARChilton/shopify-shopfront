8942504616859f68e9e12de4da9652844e9d8473
Adam Chilton <adam@dsan.co.uk>
2020-06-27 22:06:28 +0100

    made "false" rather than boolean false

--- shop_theme/src/components/Search/index.jsx
+++ shop_theme/src/components/Search/index.jsx
@@ -119,7 +119,7 @@ const Search = () => {
                   type="text"
                   value={query}
                   autoFocus
-                  autoComplete={false}
+                  autoComplete="false"
                   onChange={search}
                   placeholder={searchInputLabel}
                 />
