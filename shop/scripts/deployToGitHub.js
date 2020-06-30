fff39c1eb297d0344f1747a165e3e6844315e056
Adam Chilton <adam@dsan.co.uk>
2020-06-30 20:21:39 +0100

    now process exits with failure

--- shop/scripts/deployToGitHub.js
+++ shop/scripts/deployToGitHub.js
@@ -1,16 +1,22 @@
 const GHPages = require('gh-pages')
 require('dotenv').config({ path: `.env` })
 
+const repo = process.env.GATSBY_DEMO_DEPLOY_REPO
+console.log(repo)
+if (!repo) {
+  console.warn('no env vars')
+  process.exit(1)
+}
 // replace with your repo url
 GHPages.publish(
   'public',
   {
     branch: 'gh-pages',
-    repo: `https://github.com/archilton/${process.env.GATSBY_DEMO_DEPLOY_REPO}.git`,
+    repo: `https://github.com/archilton/${repo}.git`,
   },
   () => {
     console.log(
-      `Deploy Complete! check it out here https://archilton.github.io/${process.env.GATSBY_DEMO_DEPLOY_REPO}`
+      `Deploy Complete! check it out here https://archilton.github.io/${repo}`
     )
   }
 )
