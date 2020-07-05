bb500f424b6c782bb9ed7bf8d801669e915dc379
Adam Chilton <adam@dsan.co.uk>
2020-07-05 09:56:54 +0100

    fix console log

--- shop/scripts/deployToGitHub.js
+++ shop/scripts/deployToGitHub.js
@@ -2,7 +2,7 @@ const GHPages = require('gh-pages')
 require('dotenv').config()
 
 const { GATSBY_DEMO_DEPLOY_REPO } = process.env.GATSBY_DEMO_DEPLOY_REPO
-console.log(repo)
+console.log(GATSBY_DEMO_DEPLOY_REPO)
 if (!GATSBY_DEMO_DEPLOY_REPO) {
   console.warn('no env vars')
   process.exit(1)
